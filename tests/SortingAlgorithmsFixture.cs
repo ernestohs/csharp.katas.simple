6cdb463cd7341d58febae4fa0368c4b7f20c3ee5
Ernesto Herrera Salinas <ernestohs@gmail.com>
2016-08-09 22:25:30 -0500

    Update SortingAlgorithmsFixture.cs

--- tests/SortingAlgorithmsFixture.cs
+++ tests/SortingAlgorithmsFixture.cs
@@ -11,7 +11,7 @@ namespace CSharp.Basic.Katas.Tests
         [TestCase(new[] { 'z', 'y', 'x', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a' }, new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' })]
         public void BubleSortCharacterTest(char[] original, char[] expected)
         {
-            var target = new BubleSort();
+            var target = new BubleSort(); // TODO: Use and Interface in order to re-use these tests to execute different algorithms.
 
             var actual = target.Sort(original);
 
